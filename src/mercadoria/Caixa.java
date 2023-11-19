5cd26843d252fc84eea0c40d4d266d78f76f0cce
Ângelo Garcia <fhzkngj@gmail.com>
2023-11-19 16:42:46 -0400

    Adicionar esboço de Caixa

--- src/mercadoria/Caixa.java
+++ src/mercadoria/Caixa.java
@@ -1,4 +1,28 @@
 package mercadoria;
 
+import utilitarios.TipoDeProduto;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Caixa {
+    private List<Produto> carrinho = new ArrayList<>();
+    private boolean temItemRestrito;
+    private double totalDaCompra;
+
+    /**
+     * Adiciona um produto no carrinho, soma o preço ao totalDaCompra e
+     * verifica se um item de venda somente para adultos foi adicionado
+     * para modificar a variável temItemRestrito.
+     *
+     * @param p Produto que vai ser adicionado.
+     */
+    public void adicionarProduto(Produto p) {
+        if (p.getTipo() == TipoDeProduto.ADULTO) {
+            this.temItemRestrito = true;
+        }
+
+        this.totalDaCompra += p.getPreco();
+
+        this.carrinho.add(p);
+    }
 }
