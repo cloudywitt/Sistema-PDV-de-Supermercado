a44ef8945e9c83852ca0c5ed00ca00b9ae0ea93d
Ângelo Garcia <fhzkngj@gmail.com>
2023-11-19 16:19:28 -0400

    Adicionar atributos, construtor e sobrescrever toString de Produtos

--- src/mercadoria/Produto.java
+++ src/mercadoria/Produto.java
@@ -1,4 +1,27 @@
 package mercadoria;
 
-public abstract class Produto {
+import utilitarios.TipoDeProduto;
+
+public class Produto {
+    private final String descricao;
+    private final long codigoDeBarras;
+    private final double preco;
+    private final TipoDeProduto tipo;
+    private int quantidade = 1;
+
+    public Produto(String descricao, long codigoDeBarras, double preco, TipoDeProduto tipo) {
+        this.descricao = descricao;
+        this.codigoDeBarras = codigoDeBarras;
+        this.preco = preco;
+        this.tipo = tipo;
+    }
+
+    @Override
+    public String toString() {
+        return  quantidade + "x " +
+                "Descrição: " + descricao + " " +
+                "Código: " + codigoDeBarras + " " +
+                "Tipo: " + tipo + " " +
+                "Preço: R$" + preco;
+    }
 }
