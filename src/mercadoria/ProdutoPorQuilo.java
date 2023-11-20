fc872cedf88db2b8e50584bf724dbc4d1e0756b0
Ângelo Garcia <fhzkngj@gmail.com>
2023-11-19 23:34:00 -0400

    Adicionar descrição de ProdutoPorQuilo

--- src/mercadoria/ProdutoPorQuilo.java
+++ src/mercadoria/ProdutoPorQuilo.java
@@ -1,4 +1,19 @@
 package mercadoria;
 
+import utilitarios.TipoDeProduto;
+
 public class ProdutoPorQuilo extends Produto {
+    private final double precoPorQuilo;
+    private final double peso;
+
+    public ProdutoPorQuilo(String descricao, long codigoDeBarras, double preco, TipoDeProduto tipo, double precoPorQuilo, double peso) {
+        super(descricao + peso, codigoDeBarras, preco, tipo);
+        this.precoPorQuilo = precoPorQuilo;
+        this.peso = peso;
+    }
+
+    @Override
+    public double getPreco() {
+        return precoPorQuilo * peso;
+    }
 }
