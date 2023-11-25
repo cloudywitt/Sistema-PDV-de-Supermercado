b0344552c25ec69c2270e7d62c4623ec4e371041
Lucas Guilherme <141698980+ShimiraJunior@users.noreply.github.com>
2023-11-25 01:55:34 -0300

    Atualizando Pacote Usuário
    Inserindo o construtor e variáveis de acesso, métodos ainda precisam ser trabalhados.

--- src/usuarios/Gerente.java
+++ src/usuarios/Gerente.java
@@ -1,4 +1,23 @@
 package usuarios;
 
-public class Gerente {
+public class Gerente extends Funcionario {
+
+    //Construtor Padrão
+    public Gerente(String nome_usuario, String senha_usuario, int HoraEntrada, int HoraSaida, String id, String sexo, int cpf) {
+        super(nome_usuario, senha_usuario, HoraEntrada, HoraSaida, id, sexo, cpf);
+    }
+
+    //Métodos
+    
+    public void CancelarProduto() {
+        
+    }
+    
+    public void CancelarCompra() {
+        
+    }
+    
+    public void CadastrarFuncionario() {
+    
+    }
 }
