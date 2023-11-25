a81bbe9e8c6141affef66d9f95fad3fbb7b60a92
Lucas Guilherme <141698980+ShimiraJunior@users.noreply.github.com>
2023-11-25 01:33:31 -0300

    Atualizando Pacote Usuário
    Inserindo o construtor e variáveis de acesso, métodos ainda precisam ser trabalhados.

--- src/usuarios/Usuario.java
+++ src/usuarios/Usuario.java
@@ -2,17 +2,44 @@ package usuarios;
 
 import java.util.ArrayList;
 import java.util.List;
+
 class Usuario {
     private String id;
     private String senha;
-    public Usuario (String id, String senha){
+    private int cpf;
+
+    //Construtor
+    
+    public Usuario (String id, String senha, int cpf) {
         this.id = id;
         this.senha = senha;
+        this.cpf = cpf;
     }
-    public String getId() {
+    
+    //Getters and Setters
+    
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public int getId() {
         return id;
     }
-    public String getSenha(){
-        return senha;
+
+    public void setId(int id) {
+        this.id = id;
     }
+    
 }
