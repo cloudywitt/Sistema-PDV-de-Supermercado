0fccb4c85704c4e53c289ec9238ccc682d072d7e
Ângelo Garcia <fhzkngj@gmail.com>
2023-11-27 05:11:26 -0400

    Melhorar enumeração TipoDeProduto

--- src/utilitarios/TipoDeProduto.java
+++ src/utilitarios/TipoDeProduto.java
@@ -1,13 +1,32 @@
 package utilitarios;
 
 public enum TipoDeProduto {
-    ACOUGUE,
-    HORTIFRUTI,
-    LIMPEZA,
-    HIGIENE,
-    ELETRODOMESTICO,
-    PERECIVEL,
-    NAO_PERECIVEL,
-    ADULTO,
-    PARA_PET,
+    ACOUGUE(1),
+    HORTIFRUTI(2),
+    LIMPEZA(3),
+    HIGIENE(4),
+    ELETRODOMESTICO(5),
+    PERECIVEL(6),
+    NAO_PERECIVEL(7),
+    ADULTO(8),
+    PARA_PET(9);
+
+    private final int value;
+
+    TipoDeProduto(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return this.value;
+    }
+    public static TipoDeProduto getPorValor(int valor) {
+        for (TipoDeProduto tipo : TipoDeProduto.values()) {
+            if (tipo.value == valor) {
+                return tipo;
+            }
+        }
+
+        return null;
+    }
 }
